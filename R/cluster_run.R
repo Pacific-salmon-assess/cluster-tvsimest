4fa55f8e52fc4a7cca104614bf8ea29b1c65e553
caw001 <catarina.wor@dfo-mpo.gc.ca>
2023-06-16 18:06:33 -0700

    add run all tmb

--- R/cluster_run.R
+++ R/cluster_run.R
@@ -1,4 +1,99 @@
 
+#============================================================================
+#all scenarios
+
+library(rslurm)
+library(samEst)
+source("R/tmb_func.R")
+
+simPars1 <- read.csv("data/generic/SimPars.csv")
+simPars2 <- read.csv("data/sensitivity/SimPars.csv")
+simPars3 <- read.csv("data/sensitivity_halfSmax/SimPars.csv")
+simPars4 <- read.csv("data/Smax_sensitivity/SimPars.csv")
+simPars5 <- read.csv("data/Smax_sensitivity_doublealpha/SimPars.csv")
+simPars6 <- read.csv("data/sigmalow_sensitivity/SimPars.csv")
+simPars7 <- read.csv("data/sigmamed_sensitivity/SimPars.csv")
+simPars8 <- read.csv("data/genericER/SimPars_ER.csv")
+simPars9 <- read.csv("data/generic_biascorr/SimPars_biascorr.csv")
+
+
+simParsall<-rbind(simPars1,simPars2,simPars3,simPars4,simPars5,simPars6,simPars7,simPars8,simPars9)
+
+
+
+parsall<-data.frame(path="..",
+  a=rep(seq_len(nrow(simParsall)),each=1000),
+  u=1:1000)
+
+
+sjobtmball <- slurm_apply(tmb_func, parsall, jobname = 'TMBrunall',
+                    nodes = 250, cpus_per_node = 1, submit = FALSE,
+                    pkgs=c("samEst"),
+                    rscript_path = "/home/caw001/Documents/tvsimest/cluster-tvsimest",
+                    libPaths="/gpfs/fs7/dfo/hpcmc/comda/caw001/Rlib/4.1",
+                    global_objects=c("simParsall"))
+
+resall <- get_slurm_out(sjobtmball, outtype = 'table', wait = TRUE)
+
+#rowsby scenario
+nrow(simParsall)
+
+nr1<-1:nrow(simPars1)
+nr2<-nrow(simPars1)+(1:nrow(simPars2))
+nr3<-nrow(simPars1)+nrow(simPars2)+(1+nrow(simPars3))
+nr4<-nrow(simPars1)+nrow(simPars2)+nrow(simPars3)+(1+nrow(simPars4))
+nr5<-nrow(simPars1)+nrow(simPars2)+nrow(simPars3)+nrow(simPars4)+(1+nrow(simPars5))
+nr6<-nrow(simPars1)+nrow(simPars2)+nrow(simPars3)+nrow(simPars4)+nrow(simPars5)+(1+nrow(simPars6))
+nr7<-nrow(simPars1)+nrow(simPars2)+nrow(simPars3)+nrow(simPars4)+nrow(simPars5)+nrow(simPars6)+(1+nrow(simPars7))
+nr8<-nrow(simPars1)+nrow(simPars2)+nrow(simPars3)+nrow(simPars4)+nrow(simPars5)+nrow(simPars6)+nrow(simPars7)+(1+nrow(simPars8))
+nr9<-nrow(simPars1)+nrow(simPars2)+nrow(simPars3)+nrow(simPars4)+nrow(simPars5)+nrow(simPars6)+nrow(simPars7)+nrow(simPars8)+(1+nrow(simPars9))
+
+#base
+saveRDS(resall[resall$scenario%in%simPar1$scenario,], file = "resbase.rds")
+saveRDS(resall[resall$scenario%in%simPars1$scenario[seq_len(nrow(simPars1)/2)],],], file = "resbase1.rds")
+saveRDS(resall[resall$scenario%in%simPars1$scenario[(nrow(simPars1)/2+1):nrow(simPars1)],],], file = "resbase2.rds")
+
+#sensitivity a
+saveRDS(resall[resall$scenario%in%simPars2$scenario,], file = "res_a.rds")
+saveRDS(resall[resall$scenario%in%simPars2$scenario[seq_len(nrow(simPars2)/2)],], file = "res_a1.rds")
+saveRDS(resall[resall$scenario%in%simPars2$scenario[(nrow(simPars2)/2+1):nrow(simPars2)],], file = "res_a2.rds")
+
+#sensitivity a scenarios half smax
+saveRDS(resall[resall$scenario%in%simPars3$scenario,], file = "res_asmax.rds")
+saveRDS(resall[resall$scenario%in%simPars3$scenario[seq_len(nrow(simPars3)/2)],], file = "res_asmax1.rds")
+saveRDS(resall[resall$scenario%in%simPars3$scenario[(nrow(simPars3)/2+1):nrow(simPars3)],], file = "res_asmax2.rds")
+
+#smax scenarios 
+saveRDS(resall[resall$scenario%in%simPars4$scenario,], file = "res_smax.rds")
+saveRDS(resall[resall$scenario%in%simPars4$scenario[seq_len(nrow(simPars4)/2)],], file = "res_smax1.rds")
+saveRDS(resall[resall$scenario%in%simPars4$scenario[(nrow(simPars4)/2+1):nrow(simPars4)],], file = "res_smax2.rds")
+
+#smax scenarios double alpha
+saveRDS(resall[resall$scenario%in%simPars5$scenario,], file = "res_smaxda.rds")
+saveRDS(resall[resall$scenario%in%simPars5$scenario[seq_len(nrow(simPars5)/2)],], file = "res_smaxda1.rds")
+saveRDS(resall[resall$scenario%in%simPars5$scenario[(nrow(simPars5)/2+1):nrow(simPars5)],], file = "res_smaxda2.rds")
+
+#sensitivity sigma scenarios low
+saveRDS(resall[resall$scenario%in%simPars6$scenario,], file = "res_siglow.rds")
+saveRDS(resall[resall$scenario%in%simPars6$scenario[seq_len(nrow(simPars6)/2)],], file = "res_siglow1.rds")
+saveRDS(resall[resall$scenario%in%simPars6$scenario[(nrow(simPars6)/2+1):nrow(simPars6)],], file = "res_siglow2.rds")
+
+#sensitivity sigma scenarios med
+saveRDS(resall[resall$scenario%in%simPars7$scenario,], file = "res_sigmed.rds")
+saveRDS(resall[resall$scenario%in%simPars7$scenario[seq_len(nrow(simPars7)/2)],], file = "res_sigmed1.rds")
+saveRDS(resall[resall$scenario%in%simPars7$scenario[(nrow(simPars7)/2+1):nrow(simPars7)],], file = "res_sigmed2.rds")
+
+
+#sensitivity baseER
+saveRDS(resall[resall$scenario%in%simPars8$scenario,], file = "resbase_ER.rds")
+saveRDS(resall[resall$scenario%in%simPars8$scenario[seq_len(nrow(simPars8)/2)],], file = "resbase_ER1.rds")
+saveRDS(resall[resall$scenario%in%simPars8$scenario[(nrow(simPars8)/2+1):nrow(simPars8)],], file = "resbase_ER2.rds")
+
+
+#sensitivity base_biascorr
+saveRDS(resall[resall$scenario%in%simPars9$scenario,], file = "resbase_biascorr.rds")
+saveRDS(resall[resall$scenario%in%simPars9$scenario[seq_len(nrow(simPars9)/2)],], file = "resbase_biascorr1.rds")
+saveRDS(resall[resall$scenario%in%simPars9$scenario[(nrow(simPars9)/2+1):nrow(simPars9)],], file = "resbase_biascorr2.rds")
 
 #============================================================================
 #sbase case scenarios
@@ -373,211 +468,3 @@ saveRDS(result_sigmed_73, file = "res_sigmed_73.rds")
 
 
 #===========================================================================================
-#---------------------------------------------------------------------------------------------------------
-#stan version
-library(cmdstanr)
-library(rslurm)
-library(samEst)
-source("R/stan_func.R")
-
-
-file1=file.path(cmdstanr::cmdstan_path(),'sr models', "m1f.stan")
-mod1=cmdstanr::cmdstan_model(file1)
-file2=file.path(cmdstanr::cmdstan_path(),'sr models', "m2f.stan")
-mod2=cmdstanr::cmdstan_model(file2)
-file3=file.path(cmdstanr::cmdstan_path(),'sr models', "m3f.stan")
-mod3=cmdstanr::cmdstan_model(file3)
-file4=file.path(cmdstanr::cmdstan_path(),'sr models', "m4f.stan")
-mod4=cmdstanr::cmdstan_model(file4)
-file5=file.path(cmdstanr::cmdstan_path(),'sr models', "m5f.stan")
-mod5=cmdstanr::cmdstan_model(file5)
-file6=file.path(cmdstanr::cmdstan_path(),'sr models', "m6f.stan")
-mod6=cmdstanr::cmdstan_model(file6)
-file7=file.path(cmdstanr::cmdstan_path(),'sr models', "m7f.stan")
-mod7=cmdstanr::cmdstan_model(file7)
-file8=file.path(cmdstanr::cmdstan_path(),'sr models', "m8f.stan")
-mod8=cmdstanr::cmdstan_model(file8)
-
-
-simPars <- read.csv("data/generic/SimPars.csv")
-
-tst <- stan_func(path=".",
-  a=1,
-  u=1)
-
-
-#I can run a maximum of 6 scenarios at a time, otherwise R cannot run the results in. 
-
-#base case scn 1-6
-pars<-data.frame(path="..",
-  a=rep(seq_len(nrow(simPars)/2),each=10),
-  u=1:10)
-
-sjobstan <- slurm_apply(stan_func, pars, jobname = 'stanrun',
-                    nodes = 50, cpus_per_node = 1, submit = FALSE,
-                    pkgs=c("samEst", "cmdstanr","rlang"),
-                    rscript_path = "/fs/vnas_Hdfo/comda/dag004/homey/cluster-tvsimest/",
-                    libPaths="/gpfs/fs7/dfo/hpcmc/comda/dag004/Rlib/4.1",
-                    global_objects=c("simPars", "mod1", "mod2", "mod3",
-                      "mod4","mod5","mod6","mod7","mod8"))
-
-
-
-#AFTER JOB IS DONE IMPORT  the results
-resstan <- get_slurm_out(sjobstan, outtype = 'table', wait = FALSE)
-
-saveRDS(resstan, file = "resstan.rds")
-saveRDS(resstan[resstan$scenario%in%simPars$scenario[seq_len(nrow(simPars)/2)],], file = "resstan1.rds")
-saveRDS(resstan[resstan$scenario%in%simPars$scenario[(nrow(simPars)/2+1):nrow(simPars)],], file = "resstan2.rds")
-
-head(resstan, 3)
-
-
-
-cleanup_files(sjobstan)
-
-
-#base case scn 7-12
- 
-
-pars<-data.frame(path="..",
-  a=rep((nrow(simPars)/2):nrow(simPars),each=1000),
-  u=1:1000)
- 
-
-sjobstan2 <- slurm_apply(stan_func, pars, jobname = 'stanrun',
-                    nodes = 300, cpus_per_node = 1, submit = FALSE,
-                    pkgs=c("samEst", "cmdstanr"),
-                    rscript_path = "/home/caw001/Documents/tvsimest/cluster-tvsimest",
-                    libPaths="/gpfs/fs7/dfo/hpcmc/comda/caw001/Rlib/4.1",
-                    global_objects=c("simPars", "mod1", "mod2", "mod3",
-                      "mod4","mod5","mod6","mod7","mod8"))
-
-
-
-#AFTER JOB IS DONE IMPORT  the results
-resstan2 <- get_slurm_out(sjobstan2, outtype = 'table', wait = FALSE)
-head(resstan2, 3)
-
-
-saveRDS(resstan, file = "resstan7_12.rds")
-
-cleanup_files(sjobstan)
-
-
-
-#stan lfo runs####
-library(rslurm)
-library(samEst)
-source("R/stan_lfo_func.R") #stan lfo function
-library(cmdstanr)
-
-#load in cmdstanr models for LFO
-file1=file.path(cmdstanr::cmdstan_path(),'srmodels', "m1loo.stan")
-mod1lfo=cmdstanr::cmdstan_model(file1)
-file2=file.path(cmdstanr::cmdstan_path(),'srmodels', "m2loo.stan")
-mod2lfo=cmdstanr::cmdstan_model(file2)
-file3=file.path(cmdstanr::cmdstan_path(),'srmodels', "m3loo.stan")
-mod3lfo=cmdstanr::cmdstan_model(file3)
-file4=file.path(cmdstanr::cmdstan_path(),'srmodels', "m4loo.stan")
-mod4lfo=cmdstanr::cmdstan_model(file4)
-file5=file.path(cmdstanr::cmdstan_path(),'srmodels', "m5loo.stan")
-mod5lfo=cmdstanr::cmdstan_model(file5)
-file6=file.path(cmdstanr::cmdstan_path(),'srmodels', "m6loo.stan")
-mod6lfo=cmdstanr::cmdstan_model(file6)
-file7=file.path(cmdstanr::cmdstan_path(),'srmodels', "m7loo.stan")
-mod7lfo=cmdstanr::cmdstan_model(file7)
-file8=file.path(cmdstanr::cmdstan_path(),'srmodels', "m8loo.stan")
-mod8lfo=cmdstanr::cmdstan_model(file8)
-
-#simulation parameters
-simPars <- read.csv("data/generic/SimPars.csv")
-
-#test function- this takes awhile....
-#tst <- stan_lfo(path=".",
-#                a=1,
-#                u=1)
-
-#test some pars
-pars<-data.frame(path="..",
-                   a=rep(seq_len(nrow(simPars)),each=5),
-                   u=1:5)
-#slurm job
-sjobstanloobase <- slurm_apply(stan_lfo, pars, jobname = 'stanloobase',
-                            nodes = 25, cpus_per_node = 1, submit = FALSE,
-                            pkgs=c("cmdstanr", "samEst"),
-                            rscript_path = "/gpfs/fs7/dfo/hpcmc/comda/caw001/results/cluster-tvsimest/",
-                            libPaths="/gpfs/fs7/dfo/hpcmc/comda/caw001/Rlib/4.1",
-                            global_objects=c("simPars", "mod1lfo", "mod2lfo", "mod3lfo",
-                                             "mod4lfo","mod5lfo","mod6lfo","mod7lfo","mod8lfo"))
-
-
-resstanloo <- get_slurm_out(sjobstanloobase, outtype = 'table', wait = FALSE)
-
-saveRDS(resstan, file = "resstan.rds")
-saveRDS(resstan[resstan$scenario%in%simPars$scenario[seq_len(nrow(simPars)/2)],], file = "resstan1.rds")
-saveRDS(resstan[resstan$scenario%in%simPars$scenario[(nrow(simPars)/2+1):nrow(simPars)],], file = "resstan2.rds")
-
-
-#================================================================================================================
-#empirical - lfo stan runs####
-library(cmdstanr)
-library(rslurm)
-library(samEst)
-source("R/emp_lfo.R")
-
-data<- read.csv("data/emp/salmon_productivity_compilation_may2023.csv")
-stocks<- read.csv("data/emp/all_stocks_info_may2023.csv")
-
-###Load in data####
-set_cmdstan_path("/fs/vnas_Hdfo/comda/dag004/.cmdstan/cmdstan-2.31.0")
-file1lfo=file.path(cmdstanr::cmdstan_path(),'sr models', "m1loo.stan")
-mod1lfo=cmdstanr::cmdstan_model(file1lfo)
-file2lfo=file.path(cmdstanr::cmdstan_path(),'sr models', "m2loo.stan")
-mod2lfo=cmdstanr::cmdstan_model(file2lfo)
-file3lfo=file.path(cmdstanr::cmdstan_path(),'sr models', "m3loo.stan")
-mod3lfo=cmdstanr::cmdstan_model(file3lfo)
-file4lfo=file.path(cmdstanr::cmdstan_path(),'sr models', "m4loo.stan")
-mod4lfo=cmdstanr::cmdstan_model(file4lfo)
-file5lfo=file.path(cmdstanr::cmdstan_path(),'sr models', "m5loo.stan")
-mod5lfo=cmdstanr::cmdstan_model(file5lfo)
-file6lfo=file.path(cmdstanr::cmdstan_path(),'sr models', "m6loo.stan")
-mod6lfo=cmdstanr::cmdstan_model(file6lfo)
-file7lfo=file.path(cmdstanr::cmdstan_path(),'sr models', "m7loo.stan")
-mod7lfo=cmdstanr::cmdstan_model(file7lfo)
-file8lfo=file.path(cmdstanr::cmdstan_path(),'sr models', "m8loo.stan")
-mod8lfo=cmdstanr::cmdstan_model(file8lfo)
-
-
-#Remove stocks with less than 15 years of recruitment data
-stocks_f=subset(stocks,n.years>=16) #264 stocks
-stocks_f$stock.name=gsub('/','_',stocks_f$stock.name)
-stocks_f$stock.name=gsub('&','and',stocks_f$stock.name)
-
-data_f=subset(data,stock.id %in% stocks_f$stock.id)
-length(unique(data_f$stock.id)) #264
-stocks_f$stock.id2=seq(1:nrow(stocks_f))
-data_f$stock.id2=stocks_f$stock.id2[match(data_f$stock.id,stocks_f$stock.id)]
-
-if(any(data_f$spawners==0)){data_f$spawners=data_f$spawners+1;data_f$logR_S=log(data_f$recruits/data_f$spawners)}
-if(any(data_f$recruits==0)){data_f$recruits=data_f$recruits+1;data_f$logR_S=log(data_f$recruits/data_f$spawners)}
-data_f$logR_S=log(data_f$recruits/data_f$spawners)
-
-pars=data.frame(u=1:nrow(stocks_f))
-
-sjobstan_emp <- slurm_apply(emp_lfo, pars, jobname = 'emprun',
-                         nodes = 300, cpus_per_node = 1, submit = FALSE,
-                         pkgs=c("samEst", "cmdstanr"),
-                         rscript_path = "/home/dag004/Documents/cluster-tvsimest",
-                         libPaths="/gpfs/fs7/dfo/hpcmc/comda/dag004/Rlib/4.1",
-                         global_objects=c("data_f","stocks_f", "mod1lfo", "mod2lfo", "mod3lfo",
-                                          "mod4lfo","mod5lfo","mod6lfo","mod7lfo","mod8lfo"))
-
-#log_a sensitivity
-
-#log_a sensitivity - half smax
-
-#Smax sensitivity
-
-#Smax sensitivity
-
