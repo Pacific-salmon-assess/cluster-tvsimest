38ceb9e773f8815e0ce909ed1ab981f5b833bdd9
caw001 <catarina.wor@dfo-mpo.gc.ca>
2023-07-31 17:09:13 -0700

    new stanfunc

--- R/cluster_run_stanCW.R
+++ R/cluster_run_stanCW.R
@@ -51,7 +51,7 @@ pars<-data.frame(path="..",
 tst<-stan_func(path=".", a=2,u=19)
 
 sjobstan <- slurm_apply(stan_func, pars, jobname = 'stanrunhi',
-                    nodes = 120, cpus_per_node = 1, submit = FALSE,
+                    nodes = 250, cpus_per_node = 1, submit = FALSE,
                     pkgs=c("samEst", "cmdstanr"),
                     rscript_path = "/gpfs/fs7/dfo/hpcmc/comda/caw001/results/cluster-tvsimest/",
                     libPaths="/gpfs/fs7/dfo/hpcmc/comda/caw001/Rlib/4.1",
