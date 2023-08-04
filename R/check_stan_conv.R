23afbc858e29fd4c8835ae78479a0bf05bded231
caw001 <catarina.wor@dfo-mpo.gc.ca>
2023-08-04 15:18:40 -0700

    edit stan conv

--- R/check_stan_conv.R
+++ R/check_stan_conv.R
@@ -1,14 +1,14 @@
 check_stan_conv<- function(stansum){
   #stansum=resf4
 
-  rhat <- sum((stansum$rhat-1)>.005)
+  rhat <- sum((stansum$rhat-1)>.01)
   esstail <- stansum$ess_tail<400
   essbulk <- stansum$ess_bulk<400
 
 
 
   conv_mat <- data.frame(variable=stansum$variable,
-    rhat=as.numeric((stansum$rhat-1)>.005),
+    rhat=as.numeric((stansum$rhat-1)>.01),
     esstail=as.numeric(esstail),
     essbulk=as.numeric(essbulk)    ) 
 
@@ -30,14 +30,14 @@ check_stan_conv<- function(stansum){
 check_tmbstan_conv<- function(stansum){
   #stansum=resf4
 
-  rhat <- sum((stansum[,colnames(stansum)=="Rhat"]-1)>.005)
+  rhat <- sum((stansum[,colnames(stansum)=="Rhat"]-1)>.01)
   ess <- stansum[,colnames(stansum)=="n_eff"]<400
   
 
   conv_mat <- data.frame(variable=rownames(stansum),
-    rhat=as.numeric((stansum[,colnames(stansum)=="Rhat"]-1)>.005),
+    rhat=as.numeric((stansum[,colnames(stansum)=="Rhat"]-1)>.01),
     ess=as.numeric(ess),
-    sumconv=as.numeric((stansum[,colnames(stansum)=="Rhat"]-1)>.005)+as.numeric(ess)
+    sumconv=as.numeric((stansum[,colnames(stansum)=="Rhat"]-1)>.01)+as.numeric(ess)
     ) 
 
   return(list(convergence=sum(rhat,sum(ess)),
